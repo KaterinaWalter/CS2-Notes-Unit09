a0061092f53e9ff58595383123fe99ff71fd29ef
Katerina Walter <94501766+katerinanavab@users.noreply.github.com>
2025-03-06 13:56:42 +0000

    7.2 notes

--- Vehicle.java
+++ Vehicle.java
@@ -9,4 +9,22 @@ public class Vehicle {
     private double avgSpeed;
     private String color;
 
+    // 2. CONSTRUCTORS
+    // Methods to INITIALIZE all variables
+    public Vehicle() {
+        this.numWheels = 4;
+        this.avgSpeed = 60;
+        this.color = "White";
+    }
+    // Constructor with formal parameters
+    public Vehicle(int numWheels, double avgSpeed, String color) {
+        // INITIALIZE to passed arguments
+        this.numWheels = numWheels;
+        this.avgSpeed = avgSpeed;
+        this.color = color;
+    }
+    
+
+
+
 }
